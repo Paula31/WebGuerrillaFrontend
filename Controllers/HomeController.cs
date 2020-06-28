31ef48de39e7dbd8f8cca4013b6b01c4db49e1e5
JesusTorresRodriguez <felipe-torres23@hotmail.com>
2020-06-27 22:27:51 -0600

    Se grega model y se conecta vista con controller de Regitrarse

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebGuerrillaFrontEnd.Models;
 
 namespace WebGuerrillaFrontEnd.Controllers
 {
@@ -19,7 +21,32 @@ namespace WebGuerrillaFrontEnd.Controllers
         {
             ViewBag.Title = "Registrarse";
 
+
+
+            //var httpClient = new HttpClient();
+            //var json = await httpClient.GetStringAsync("http://192.168.100.38:51438/guerrilla");
+            //var guerrillas = JsonConvert.DeserializeObject<List<Guerrilla>>(json);
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrarse(FormCollection formCollection)
+        {
+            ViewBag.Title = "Registrarse";
+
+            Guerrilla guerrilla = new Guerrilla();
+            guerrilla.GuerrillaName = formCollection["guerrillaName"];
+            guerrilla.Faction = formCollection["faction"];
+            guerrilla.Email = formCollection["email"];
+            var json = JsonConvert.SerializeObject(guerrilla);
+
+
+            //var httpClient = new HttpClient();
+            //var json = await httpClient.GetStringAsync("http://192.168.100.38:51438/guerrilla");
+            //var guerrillas = JsonConvert.DeserializeObject<List<Guerrilla>>(json);
+
+            return RedirectToActionPermanent("Registrarse");
         }
 
         public ActionResult Batalla()
---
+++ Models/Buildings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebGuerrillaFrontEnd.Models
+{
+	public class Buildings
+	{
+		public Buildings(int bunker)
+		{
+			Bunker = bunker;
+		}
+
+		public Buildings()
+		{
+		}
+		private int bunker = 0;
+		public int Bunker
+		{
+			get
+			{
+				return bunker;
+			}
+			set
+			{
+				if (value < 0) throw new Exception("Los en building debe ser positivo.");
+				bunker = value;
+			}
+		}
+
+	}
+}
