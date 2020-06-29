b16ae96a9e980395a2815afd8cb89bf3d2fdfa34
JesusTorresRodriguez <felipe-torres23@hotmail.com>
2020-06-28 20:32:20 -0600

    Se agregan funciones de perfil index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using WebGuerrillaFrontEnd.Models;
@@ -10,22 +13,21 @@ namespace WebGuerrillaFrontEnd.Controllers
 {
     public class HomeController : Controller
     {
+        static String IPAdrress = "http://192.168.100.38:51438";
+        static GuerrillaCompleted guerrilla;
+
+        [HttpGet]
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
         }
+	
 
-        public ActionResult Registrarse()
+		public ActionResult Registrarse()
         {
             ViewBag.Title = "Registrarse";
-
-
-
-            //var httpClient = new HttpClient();
-            //var json = await httpClient.GetStringAsync("http://192.168.100.38:51438/guerrilla");
-            //var guerrillas = JsonConvert.DeserializeObject<List<Guerrilla>>(json);
 
             return View();
         }
@@ -35,18 +37,25 @@ namespace WebGuerrillaFrontEnd.Controllers
         {
             ViewBag.Title = "Registrarse";
 
-            Guerrilla guerrilla = new Guerrilla();
-            guerrilla.GuerrillaName = formCollection["guerrillaName"];
-            guerrilla.Faction = formCollection["faction"];
-            guerrilla.Email = formCollection["email"];
-            var json = JsonConvert.SerializeObject(guerrilla);
+			try
+			{
+                Guerrilla guerrilla = new Guerrilla();
+                guerrilla.GuerrillaName = formCollection["guerrillaName"];
+                guerrilla.Faction = formCollection["faction"];
+                guerrilla.Email = formCollection["email"];
 
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(IPAdrress);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.PostAsJsonAsync("/guerrilla/" + guerrilla.GuerrillaName, guerrilla).Result;
 
-            //var httpClient = new HttpClient();
-            //var json = await httpClient.GetStringAsync("http://192.168.100.38:51438/guerrilla");
-            //var guerrillas = JsonConvert.DeserializeObject<List<Guerrilla>>(json);
+                return RedirectToAction("Index");
+            }
+			catch
+			{
+                return RedirectToActionPermanent("Registrarse");
+            }
 
-            return RedirectToActionPermanent("Registrarse");
         }
 
         public ActionResult Batalla()
@@ -70,12 +79,59 @@ namespace WebGuerrillaFrontEnd.Controllers
             return View();
         }
 
-       
-            public ActionResult Perfil()
+       [HttpGet]
+        public async Task <ActionResult> Perfil()
         {
             ViewBag.Title = "Perfil";
 
-            return View();
+            string name = Request.QueryString["GuerrillaName"];
+
+			try
+			{
+                var httpClient = new HttpClient();
+                var json = await httpClient.GetStringAsync(IPAdrress+"/guerrilla/" + name);
+                guerrilla = JsonConvert.DeserializeObject<GuerrillaCompleted>(json);
+                ViewData["guerrilla"] = guerrilla;
+                return View();
+
+            }
+            catch
+			{
+                return RedirectToActionPermanent("Index");
+            }
         }
+
+        [HttpPost]
+        public ActionResult Perfil(FormCollection formCollection)
+        {
+            ViewBag.Title = "Perfil";
+
+
+            try
+            {
+                Army army = new Army();
+                Buildings buildings = new Buildings();
+
+                army.Assault =Convert.ToInt32( formCollection["Army.Assault"]);
+                army.Engineer = Convert.ToInt32(formCollection["Army.Engineer"]);
+                army.Tank = Convert.ToInt32(formCollection["Army.Tank"]);
+                buildings.Bunker = Convert.ToInt32(formCollection["Army.Assault"]);
+                guerrilla.Army = army;
+                guerrilla.Buildings = buildings;
+
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(IPAdrress);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.PutAsJsonAsync("/guerrilla/" + guerrilla.Name + "/units", guerrilla).Result;
+
+                return RedirectToAction("Perfil", new { GuerrillaName = guerrilla.Name });
+
+            }
+            catch
+            {
+                return RedirectToActionPermanent("Perfil", new { GuerrillaName = "Thor" });
+            }
+        }
+
     }
 }
